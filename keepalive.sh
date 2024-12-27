7ef82e746ee914ba635c3e5d23fa2b36a0b3bfc7
jilin <jilin6654@gmail.com>
2024-12-27 21:48:14 +0800

    修改保活脚本

--- keepalive.sh
+++ keepalive.sh
@@ -74,58 +74,12 @@ autoUpdate() {
     #重新给各个脚本赋权限
     chmod +x ./start.sh
     chmod +x ./keepalive.sh
-    chmod +x ${installpath}/serv00-play/vless/start.sh
-    chmod +x ${installpath}/serv00-play/singbox/start.sh
-    chmod +x ${installpath}/serv00-play/singbox/killsing-box.sh
     chmod +x ${installpath}/serv00-play/ssl/cronSSL.sh
   fi
   makeMsgConfig
 }
 
 
-startNeZhaAgent() {
-  local workedir="${installpath}/serv00-play/nezha"
-  cd ${workedir}
-  local config="nezha.json"
-  if [[ ! -e "$config" ]]; then
-    red "未安装哪吒探针，请先进行安装！"
-    return 1
-  fi
-  nezha_domain=$(jq -r ".nezha_domain" $config)
-  nezha_port=$(jq -r ".nezha_port" $config)
-  nezha_pwd=$(jq -r ".nezha_pwd" $config)
-  tls=$(jq -r ".tls" $config)
-
-  if checknezhaAgentAlive; then
-    stopNeZhaAgent
-  fi
-
-  local args="--report-delay 4 --disable-auto-update --disable-force-update "
-  if [[ "$tls" == "y" ]]; then
-    args="${args} --tls "
-  fi
-
-  nohup ./nezha-agent ${args} -s "${nezha_domain}:${nezha_port}" -p "${nezha_pwd}" >/dev/null 2>&1 &
-
-}
-
-startMtg() {
-  cd ${installpath}/serv00-play/dmtg
-
-  config="config.json"
-
-  secret=$(jq -r ".secret" $config)
-  port=$(jq -r ".port" $config)
-  cmd="nohup ./mtg simple-run -n 1.1.1.1 -t 30s -a 1MB 0.0.0.0:$port $secret -c 8192 --prefer-ip=\"prefer-ipv6\" >/dev/null 2>&1 &"
-  eval "$cmd"
-  sleep 3
-  if checkMtgAlive; then
-    echo "启动成功"
-  else
-    echo "启动失败，请检查进程"
-  fi
-
-}
 
 
 startAlist() {
@@ -244,52 +198,6 @@ for obj in "${monitor[@]}"; do
         msg="webssh restarted failure."
       else
         msg="webssh restarted successfully."
-      fi
-    fi
-  elif [ "$obj" == "vmess" ]; then
-    if ! checkvmessAlive; then
-      cd ${installpath}/serv00-play/singbox
-      chmod +x ./start.sh && ./start.sh 1 keep
-      sleep 5
-      if ! checkvmessAlive; then
-        msg="vmess restarted failure."
-      else
-        msg="vmess restarted successfully."
-      fi
-    fi
-    #hy2和vmess+ws都只需要启动serv00sb，所以可以这么写
-  elif [[ "$obj" == "hy2/vmess+ws" || "$obj" == "hy2" ]]; then
-    if ! checkHy2Alive; then
-      #echo "重启serv00sb中..."
-      cd ${installpath}/serv00-play/singbox
-      chmod +x ./start.sh && ./start.sh 2 keep
-      sleep 5
-      if ! checkHy2Alive; then
-        msg="hy2 restarted failure."
-      else
-        msg="hy2 restarted successfully."
-      fi
-    fi
-  elif [ "$obj" == "nezha-agent" ]; then
-    if ! checknezhaAgentAlive; then
-      cd ${installpath}/serv00-play/nezha
-      startNeZhaAgent
-      sleep 5
-      if ! checknezhaAgentAlive; then
-        msg="nezha-agent restarted failure."
-      else
-        msg="nezha-agent restarted successfully."
-      fi
-    fi
-  elif [ "$obj" == "mtg" ]; then
-    if ! checkMtgAlive; then
-      cd ${installpath}/serv00-play/dmtg
-      startMtg
-      sleep 5
-      if ! checkMtgAlive; then
-        msg="mtproto restarted failure."
-      else
-        msg="mtproto restarted successfully."
       fi
     fi
   elif [ "$obj" == "alist" ]; then
